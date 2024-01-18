21eb2332da8777e8a19a169313dcb742bcbb283d
MiguelEq <equihuam@gmail.com>
2024-01-17 21:09:36 -0600

    Ajuste de orden

--- Ajusta_autores_blog.R
+++ Ajusta_autores_blog.R
@@ -1,11 +1,29 @@
-library(stringi)
+library(stringr)
+library(readr)
 docs <- list.files(recursive = TRUE, pattern = "qmd$", full.names = TRUE)
 
+# Ajusta autores
 for (d in docs)
 {
   txt <- read_file(file = d)
   if (str_detect(txt, "Alan Aguirre"))
     txt <- str_replace(txt, "Alan Aguirre", "Elio Lagunes")
   write_file(txt, d)
+}
+
+# Ajusta fechas
+for (d in docs)
+{
+  txt <- read_file(file = d)
+  fecha <- str_extract(txt, '(?<=date: \")([\\d\\-]*)(?=\")')
+  titulo <- str_extract(txt, '(?<=title: \")(.*?)(?=\")')
+  if (d == docs[1])
+  {
+    clases <- tibble(fecha = fecha, tit = titulo)
+  } else {
+    clases <- bind_rows(clases, c(fecha = fecha, tit = titulo))
+  }
+  clases <- clases %>% 
+            arrange(fecha)
 }
 
