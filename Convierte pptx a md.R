735d769fdb9c88798c771993b550ac13d1188a57
MiguelEq <equihuam@gmail.com>
2024-01-31 00:03:55 -0600

    DAGs

--- Convierte pptx a md.R
+++ Convierte pptx a md.R
@@ -1,7 +1,6 @@
 # Conversión de pptx a md con python
 
-
-pptx_in <- "../M3-presentaciones/experimentar.pptx"
-md_out  <- "../Blog/presentaciones/11-experimentar/experimentar.qmd"
+pptx_in <- "../Blog/presentaciones/04-diagramas-causales/Clase_2 - Diagramas Causales.pptx"
+md_out  <- "../Blog/presentaciones/04-diagramas-causales/Diagramas Causales.qmd"
 system2(command = "../pptx2md.exe", 
         args = paste0('-o "', md_out, '" "', pptx_in, '"'))
